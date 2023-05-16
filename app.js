3df3548340a3f0193d35c4a6659b8042041cf026
danizavtz <daniel.pires@virtus.ufcg.edu.br>
2023-05-16 14:14:00 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const logger = require('morgan');
+const apiMetrics = require('prometheus-api-metrics');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(logger('dev'));
+app.use(apiMetrics())
+app.get('/', (req, res) => {
+    res.status(200).json("Server up and running")
+});
+
+module.exports = app;
